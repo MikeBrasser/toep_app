c59e661ba94d147000cb242862ff8f44a609f8d3
Mike Brasser <mikebrasser2@hotmail.nl>
2021-01-26 19:44:23 +0100

    Basic topNav added

---
+++ ToepApp/app/src/main/java/com/example/toepapp/HistoryScoreFragment.kt
@@ -0,0 +1,6 @@
+package com.example.toepapp
+
+import androidx.fragment.app.Fragment
+
+class HistoryScoreFragment : Fragment() {
+}
--- ToepApp/app/src/main/java/com/example/toepapp/MainActivity.kt
+++ ToepApp/app/src/main/java/com/example/toepapp/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.toepapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var fragmentTransaction: FragmentTransaction
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadGameFragment()
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        topNav.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> Toast.makeText(applicationContext, "Clicked it", Toast.LENGTH_SHORT).show()
+                R.id.scoreboard -> Toast.makeText(applicationContext, "Clicked scoreboard", Toast.LENGTH_SHORT).show()
+                R.id.addPlayer -> Toast.makeText(applicationContext, "Clicked addPlayer", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            item
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun loadGameFragment() {
+        fragmentManager = supportFragmentManager
+        fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_target, GameFragment())
+        fragmentTransaction.commit()
     }
 }
